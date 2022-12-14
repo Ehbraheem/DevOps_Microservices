02a6db58ee81ec2bbf297e1f6c0cad0936fd7649
Bolatan Ibrahim <ehbraheem@gmail.com>
2022-12-14 17:14:52 +0100

    Feature/project (#1)
    * Add CI config
    
    * Add dockerfile
    
    * Rn docker script
    
    * Add logging
    
    * Push image to docker hub
    
    * Add kubernetes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ehbraheem/housing-ml
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo "$DOCKER_PASSWORD" | docker login -u ehbraheem --password-stdin && \
+  docker image tag housing-ml $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
