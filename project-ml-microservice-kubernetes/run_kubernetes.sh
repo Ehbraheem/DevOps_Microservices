5de0f0599f9259fbc917a6e407ecbe83eee3e489
Ehbraheem <ehbraheem@gmail.com>
2022-12-14 17:10:48 +0100

    Add kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="ehbraheem/housing-ml"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+echo "Docker ID and Image: $dockerpath"
+kubectl run housing-ml \
+    --image=$dockerpath \
+    --port=80 --labels app=housing-price-prediction
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward housing-ml 8000:80
